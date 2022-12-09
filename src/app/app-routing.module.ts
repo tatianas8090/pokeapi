d2ee9608c3146e2bf28b7b88fc3c9c4ebad4ffbf
Tatiana_Solorzano <tatianasolorzanoa@gmail.com>
2022-12-09 23:55:23 +0000

    proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { EquipoComponent } from './equipo/equipo.component';
+import { PokedexComponent } from './pokedex/pokedex.component';
+import { PokemonesComponent } from './pokemones/pokemones.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HeaderComponent},
+  { path: 'team', component: EquipoComponent},
+  { path: "buscar", component: PokedexComponent},
+  {path:"Pokemones", component: PokemonesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,10 +10,24 @@ export class HeaderComponent implements OnInit {
   public active : boolean = false
 
   constructor() { }
-  ngOnInit(): void {}
+  ngOnInit(): void{
+    const toggleButton:any = document.getElementById("button-menu");
+  const navWrapper:any = document.querySelector(".nav");
 
-  setActive() : void {
-    this.active = !this.active
+  toggleButton?.addEventListener("click", () => {
+    console.log("funcionando");
+    toggleButton.classList.toggle("close");
+    navWrapper.classList.toggle("show");
+  });
+
+  navWrapper?.addEventListener("click",  (e: { target: { id: string; }; }) => {
+    if (e.target.id === "nav") {
+      navWrapper.classList.remove("show");
+      toggleButton?.classList.remove("close");
+    }
+  });
   }
-
 }
+
+
+
